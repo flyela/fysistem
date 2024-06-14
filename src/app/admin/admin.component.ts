44d365485622b0bdbf312f239b452731fb44c094
flyela <flyela@estudiante.uniajc.edu.co>
2024-06-13 23:32:41 -0500

    Creacion de Carpetas y Rutas

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './admin.component.html',
+  styles: ``
+})
+export default class AdminComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        loadComponent: () => import('./admin/admin.component'),
+        children: [
+
+            {
+                path: 'contacto',
+                title: 'Contacto',
+                loadComponent: () => import('./admin/pages/contacto/contacto.component')
+            },
+            {
+                path: 'inicio',
+                title: 'Inicio',
+                loadComponent: () => import('./admin/pages/inicio/inicio.component')
+            },
+            {
+                path: 'nosotros',
+                title: 'Nosotros',
+                loadComponent: () => import('./admin/pages/nosotros/nosotros.component')
+            },
+            {
+                path: 'productos',
+                title: 'Productos',
+                loadComponent: () => import('./admin/pages/productos/productos.component')
+            },
+            {
+                path: 'soluciones',
+                title: 'Soluciones',
+                loadComponent: () => import('./admin/pages/soluciones/soluciones.component')
+            },
+            {
+                path:'', redirectTo:'inicio', pathMatch:'full',
+            }
+        ],
+    },
+    {
+        path:'',
+        title:'/admin',
+        pathMatch:'full'
+    }
+];
