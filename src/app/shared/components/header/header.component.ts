79b41ae39939c05fac1cadb9d22ded9510c94c32
flyela <flyela@estudiante.uniajc.edu.co>
2024-09-26 16:27:04 -0500

    configurarcion de barra de  inicio

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,33 +6,23 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
- public headerItems=routes
- .map((route)=>route.children ?? [])
- .flat()
- .filter((route)=>route && route.path)
- .filter((route)=>!route.path?.includes(':'));
+  public headerItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
+  // Variable para controlar el menú móvil
+  public isMenuOpen = false;
 
-constructor(){
-
-/*const header = routes
-  .map((route)=>route.children ?? [])
-  .flat()
-  .filter((route)=>route && route.path)
-  .filter((route)=>!route.path?.includes(':'));
-  console.log(header);*/
-}
-
-
-
-
-
-
-
+  // Método para alternar el menú
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
