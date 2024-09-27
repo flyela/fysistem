1e65acb7ad0f5709131be1c2c7a0d0d0e0ab65ad
flyela <flyela@estudiante.uniajc.edu.co>
2024-09-26 19:35:52 -0500

    ajuste inicio Carrusel

--- src/app/admin/pages/inicio/inicio.component.ts
+++ src/app/admin/pages/inicio/inicio.component.ts
@@ -10,7 +10,44 @@ import { RouterModule } from '@angular/router';
   styleUrl: './inicio.component.css'
 })
 export default class InicioComponent {
+  images = [
+    {
+      src: 'https://img.freepik.com/foto-gratis/tecnologia-equipamiento-laboratorio-azul-generado-ia_188544-19546.jpg',
+      alt: 'Handheld 3D Scanner'
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMjYHghNg0xWv-Zn0DdmrC9Uw5lHZgNwWYsg&s',
+      alt: '3D Scanning Rig'
+    },
+    {
+      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMjYHghNg0xWv-Zn0DdmrC9Uw5lHZgNwWYsg&s',
+      alt: '3D Scanning Rig'
+    }
+  ];
 
+  currentIndex = 0;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    setInterval(() => {
+      this.nextImage();
+    }, 5000); // Change image every 5 seconds
+  }
+
+  setCurrentIndex(index: number) {
+    this.currentIndex = index;
+  }
+
+  previousImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
 
 }
 
