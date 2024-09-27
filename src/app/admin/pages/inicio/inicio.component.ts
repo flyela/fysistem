eaf3a5c231b71f1581e69ffce29cfb596fbf9a67
flyela <flyela@estudiante.uniajc.edu.co>
2024-09-26 20:54:51 -0500

    ajuste de metadatos y footer

--- src/app/admin/pages/inicio/inicio.component.ts
+++ src/app/admin/pages/inicio/inicio.component.ts
@@ -13,15 +13,27 @@ export default class InicioComponent {
   images = [
     {
       src: 'https://img.freepik.com/foto-gratis/tecnologia-equipamiento-laboratorio-azul-generado-ia_188544-19546.jpg',
-      alt: 'Handheld 3D Scanner'
+      alt: 'Imagen1'
     },
     {
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMjYHghNg0xWv-Zn0DdmrC9Uw5lHZgNwWYsg&s',
-      alt: '3D Scanning Rig'
+      src: 'https://images.pexels.com/photos/19871842/pexels-photo-19871842.jpeg?cs=srgb&dl=pexels-jakubzerdzicki-19871842.jpg&fm=jpg',
+      alt: 'Imagen2'
     },
     {
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMjYHghNg0xWv-Zn0DdmrC9Uw5lHZgNwWYsg&s',
-      alt: '3D Scanning Rig'
+      src: 'https://www.stampa3dservizi.com/wp-content/uploads/2023/08/stampa-3d.webp',
+      alt: 'Imagen3'
+    },
+    {
+      src: 'https://e1.pxfuel.com/desktop-wallpaper/689/1023/desktop-wallpaper-3d-printing-start-3d-printer.jpg',
+      alt: 'Imagen4'
+    },
+    {
+      src: 'https://fbi.cults3d.com/uploads/blog/image/image/2173/visuel-impression-meilleurs-services-ligne-2021.jpg',
+      alt: 'Imagen5'
+    },
+    {
+      src: 'https://i.blogs.es/3dedda/impresora-3d-barata-en-oferta-abycubic-ideal-para-regalar-reyes-magos/500_333.jpeg',
+      alt: 'Imagen6'
     }
   ];
 
